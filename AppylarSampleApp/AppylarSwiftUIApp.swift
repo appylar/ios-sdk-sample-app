961bdb298df4614bcee478dbf379f17021755539
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2023-12-04 22:06:10 +0100

    chore: initial public release

---
+++ AppylarSampleApp/AppylarSwiftUIApp.swift
@@ -0,0 +1,82 @@
+import Appylar
+import SwiftUI
+
+@main
+struct AppylarSampleApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    init() {
+        // Set event listener before initialization
+        // Set event to show log for banner & interstitial then you have to pass delegate as self for indivisualy ads
+        /* Example: AppylarManager.setEventListener(delegate: self, bannerDelegate: self) // For Banner Ads only
+                    AppylarManager.setEventListener(delegate: self, interstitialDelegate: self) // For Interstitial Ads only
+                    AppylarManager.setEventListener(delegate: self, bannerDelegate: self, interstitialDelegate: self) // For Both Ads
+         */
+        AppylarManager.setEventListener(delegate: self, bannerDelegate: self, interstitialDelegate: self)
+
+        // Initialize
+        AppylarManager.initialize(
+            appKey: "<appKey>", // The unique app key for your app
+            adTypes: [AdType.banner, AdType.interstitial], // The ad types that you want to show
+            testMode: true // Test mode, true for development, false for production
+        )
+
+        AppylarManager.setParameters(dict: [
+            "banner_height": ["50"],
+            "age_restriction": ["18"]
+        ])
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+// Define the app delegate to keep track of the screen orientations when device is flipped
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppylarManager.supportedOrientation
+    }
+}
+
+extension AppylarSampleApp: AppylarDelegate {
+    // Event listener triggered at successful initialization
+    func onInitialized() {
+        print("onInitialized")
+    }
+
+    // Event listener triggered if an error occurs in the SDK
+    func onError(error: String) {
+        print("onError(): \(error)")
+    }
+}
+
+extension AppylarSampleApp: BannerViewDelegate {
+    // Event listener triggered when a banner is shown
+    func onBannerShown() {
+        print("onBannerShown()")
+    }
+
+    // Event listener triggered when there are no banners to show
+    func onNoBanner() {
+        print("onNoBanner()")
+    }
+}
+
+extension AppylarSampleApp: InterstitialDelegate {
+    // Event listener triggered when an interstitial is shown
+    func onInterstitialShown() {
+        print("onInterstitialShown()")
+    }
+
+    // Event listener triggered when an interstitial is closed
+    func onInterstitialClosed() {
+        print("onInterstitialClosed()")
+    }
+
+    // Event listener triggered when there are no interstitials to show
+    func onNoInterstitial() {
+        print("onNoInterstitial()")
+    }
+}
---
+++ AppylarSampleApp/ContentView.swift
@@ -0,0 +1,134 @@
+import Appylar
+import SwiftUI
+
+struct ContentView: View {
+    @State private var bannerView = BannerView()
+    @State private var isInterstitialShown = false
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    var body: some View {
+        GeometryReader { _ in
+            // Check if interstitial should be shown...
+            if isInterstitialShown {
+                // Place the interstitial view container and make it cover the whole screen
+                InterstitialViewContainer()
+                    .onReceive(NotificationCenter.default.publisher(for: .interstitialClosed)) { _ in
+                        isInterstitialShown = false
+                    }
+                    .ignoresSafeArea(.all)
+                    .frame(width: UIScreen.main.bounds.width)
+                    .frame(height: UIScreen.main.bounds.height)
+
+                // ...or if the menu should be shown
+            } else {
+                VStack {
+                    ScrollView(showsIndicators: false) {
+                        VStack(spacing: 25) {
+                            // Show Appylar logo and text
+                            Image("appylar_logo")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 40.0)
+                                .padding(.top, 70.0)
+
+                            Text("Appylar Sample App")
+                                .font(.largeTitle)
+                                .foregroundColor(Color(red: 0.16, green: 0.21, blue: 0.26))
+                                .padding()
+
+                            // Create buttons
+                            Button(action: {
+                                if bannerView.canShowAd() {
+                                    bannerView.showAd()
+                                }
+                            }) {
+                                setButtonStyle(title: "Show Banner")
+                            }
+
+                            Button(action: {
+                                bannerView.hideAd()
+                            }) {
+                                setButtonStyle(title: "Hide Banner")
+                            }
+
+                            Button(action: {
+                                if InterstitialViewController.canShowAd() {
+                                    isInterstitialShown = true
+                                }
+                            }) {
+                                setButtonStyle(title: "Show Interstitial")
+                            }
+                        }
+                    }
+
+                    // Place the banner view container at the bottom of the screen
+                    BannerViewContainer(bannerView: bannerView)
+                        .frame(height: 50)
+                }
+                .frame(maxWidth: .infinity)
+            }
+        }
+        .ignoresSafeArea(.all, edges: [.trailing, .leading])
+    }
+
+    // Set a standard button style
+    func setButtonStyle(title: String) -> some View {
+        return Text(title.uppercased())
+            .frame(width: 200.0)
+            .frame(height: 15.0)
+            .padding()
+            .background(Color(red: 0.46, green: 0.56, blue: 0.73))
+            .foregroundColor(.white)
+            .cornerRadius(8.0)
+            .disabled(isInterstitialShown)
+    }
+}
+
+// Set the container where the interstitials will be shown
+struct InterstitialViewContainer: UIViewControllerRepresentable {
+    typealias UIViewControllerType = InterstitialView
+    func makeUIViewController(context: Context) -> InterstitialView {
+        let vc = InterstitialView()
+        return vc
+    }
+
+    func updateUIViewController(_ uiViewController: InterstitialView, context: Context) {}
+}
+
+// Set the container where the banners will be shown
+struct BannerViewContainer: UIViewRepresentable {
+    let bannerView: BannerView
+    func makeUIView(context: Context) -> UIView {
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        return bannerView
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
+
+// Set notification to be able to detect when the interstitials are closed
+extension Notification.Name {
+    static let interstitialClosed = Notification.Name("interstitialClosed")
+}
+
+// Create a class that implements the InterstitialViewController class
+class InterstitialView: InterstitialViewController {
+    
+    // Override viewDidLoad() and show the interstitial
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showAd()
+    }
+
+    override func viewDidLayoutSubviews() {
+        if Session.isInterstitialShown == false {
+            NotificationCenter.default.post(name: .interstitialClosed, object: nil)
+        }
+    }
+}
+
+// Set the preview
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
